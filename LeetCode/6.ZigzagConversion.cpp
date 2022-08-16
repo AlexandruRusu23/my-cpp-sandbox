722028f9f41dc00f64e922b542c3f6f4396dc3b7
AlexandruRusu23 <alexandru.rusu707@gmail.com>
2022-08-16 20:59:13 +0300

    Zigzag Conversion problem added

---
+++ LeetCode/6.ZigzagConversion.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <vector>
+
+class Solution {
+public:
+    std::string convert(std::string s, int numRows) {
+        if (numRows == 1)
+            return s;
+        
+        std::vector<std::string> letters( numRows );
+
+        int index = 0;
+        int row = 0;
+        bool ascending = true;
+        
+        while (index < s.size())
+        {
+            letters[row].push_back(s[index]);
+            index++;
+
+            if (ascending && row < numRows - 1)
+            {
+                row++;
+                continue;
+            }
+            if (!ascending && row > 0)
+            {
+                row--;
+                continue;
+            }
+            if (row == numRows - 1)
+            {
+                row--;
+                ascending = false;
+                continue;
+            }
+            if (row == 0)
+            {
+                row++;
+                ascending = true;
+                continue;
+            }            
+        }
+        
+        std::string output = "";
+        for ( int i = 0; i < letters.size(); i++ )
+        {
+            output += letters[ i ];
+        }
+
+        return output;
+    }
+};
+
