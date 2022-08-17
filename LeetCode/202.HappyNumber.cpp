7144d63d819d1ed4772d20651594c08ecc5d79de
AlexandruRusu23 <alexandru.rusu707@gmail.com>
2022-08-18 01:02:27 +0300

    Happy Number problem added

---
+++ LeetCode/202.HappyNumber.cpp
@@ -0,0 +1,33 @@
+
+class Solution {
+public:
+    int getSum(int n)
+    {
+        int output = 0;
+        while (n)
+        {
+            int digit = n%10;
+            output += digit * digit;
+            n/=10;
+        }
+        
+        return output;
+    }
+    
+    bool isHappy(int n) {
+        int slow, fast;
+        slow = getSum(n);
+        fast = getSum(slow);
+        
+        while (slow != fast)
+        {
+            slow = getSum(slow);
+            fast = getSum(getSum(fast));
+        }
+        
+        if (slow == 1)
+            return true;
+        return false;
+    }
+};
+
