95143954790aebd002a941e0d3323b037f5a6d38
AlexandruRusu23 <alexandru.rusu707@gmail.com>
2022-08-19 19:48:44 +0300

    Container With Most Water

---
+++ LeetCode/0011.ContainerWithMostWater.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <vector>
+
+class Solution {
+public:
+    int maxArea(std::vector<int>& height) {
+        int output = 0;
+        int left = 0;
+        int right = height.size() - 1;
+        
+        while (left < right)
+        {
+            if (height[left] < height[right])
+            {
+                output = std::max(output, height[left] * (right - left));
+                left++;
+            }
+            else
+            {
+                output = std::max(output, height[right] * (right - left));
+                right--;
+            }
+        }
+        
+        return output;
+    }
+};
+
