f3d9a3607f3e41a562184321a402621fecb10b36
AlexandruRusu23 <alexandru.rusu707@gmail.com>
2022-09-14 00:18:32 +0300

    Sqrt(x) problem added

---
+++ LeetCode/0069.Sqrt(x).cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) return 0;
+        
+        int output = 1;
+        
+        while (output * output <= x)
+        {
+            if (output * output == x)
+                return output;
+            
+            output++;
+            
+            if (x / output < output)
+                break;
+        }
+        
+        return output-1;
+    }
+};
+
