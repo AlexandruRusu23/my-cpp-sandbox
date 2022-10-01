d7243af351efa4e40a6c9286489e19cdf49505d2
AlexandruRusu23 <alexandru.rusu707@gmail.com>
2022-10-02 02:04:38 +0300

    Pb. 278 and Pb. 283 added

---
+++ LeetCode/0283.MoveZeroes.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+class Solution
+{
+public:
+    void moveZeroes(std::vector<int> &nums)
+    {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        for (int i = k; i < nums.size(); i++)
+        {
+            nums[i] = 0;
+        }
+    }
+};
+
